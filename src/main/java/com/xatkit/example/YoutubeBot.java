c7b8a3cd6777f5ba76d31d72d2c0d6ab33c183b7
kradow99 <>
2021-01-11 18:15:13 +0100

    Added Utils folder and messages with thumbnail

--- src/main/java/com/xatkit/example/YoutubeBot.java
+++ src/main/java/com/xatkit/example/YoutubeBot.java
@@ -1,6 +1,10 @@
 package com.xatkit.example;
 
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.services.youtube.YouTube;
 import com.xatkit.core.XatkitBot;
+import com.xatkit.example.utils.Video;
+import com.xatkit.execution.StateContext;
 import com.xatkit.plugins.react.platform.ReactPlatform;
 import com.xatkit.plugins.react.platform.io.ReactEventProvider;
 import com.xatkit.plugins.react.platform.io.ReactIntentProvider;
@@ -8,6 +12,11 @@ import lombok.val;
 import org.apache.commons.configuration2.BaseConfiguration;
 import org.apache.commons.configuration2.Configuration;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+import static com.xatkit.dsl.DSL.any;
+import static com.xatkit.dsl.DSL.integer;
 import static com.xatkit.dsl.DSL.eventIs;
 import static com.xatkit.dsl.DSL.fallbackState;
 import static com.xatkit.dsl.DSL.intent;
@@ -15,81 +24,67 @@ import static com.xatkit.dsl.DSL.intentIs;
 import static com.xatkit.dsl.DSL.model;
 import static com.xatkit.dsl.DSL.state;
 
+import static com.xatkit.example.utils.ResponseParser.*;
+import static com.xatkit.example.YoutubeAPI.*;
+
 /**
  * This is an example greetings bot designed with Xatkit.
  * <p>
  * You can check our <a href="https://github.com/xatkit-bot-platform/xatkit/wiki">wiki</a>
  * to learn more about bot creation, supported platforms, and advanced usage.
  */
-public class MyBot {
+public class YoutubeBot {
+
+    private static void replyYoutubeBot(StateContext context, ReactPlatform reactPlatform, String response, String keyword) {
+        Video[] data = getData(response);
+        String msg = "I searched \"" + keyword + "\" and I found these videos for you...\n";
+        reactPlatform.reply(context, msg);
+
+        for (Video video : data) {
+            reactPlatform.replyLinkSnippet(context, video.getVideoTitle(), video.getVideoURL(), video.getThumbnailURL());
+        }
+    }
 
     /*
      * Your bot is a plain Java application: you need to define a main method to make the created jar executable.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+            throws GeneralSecurityException, IOException, GoogleJsonResponseException {
+
+        YouTube youtubeService = getService();
 
         /*
          * Define the intents our bot will react to.
-         * <p>
-         * In this example we want our bot to answer greetings inputs and "how are you" questions, so we create an
-         * intent for each, and we give a few example training sentences to configure the underlying NLP engine.
-         * <p>
-         * Note that we recommend the usage of Lombok's val when using the Xatkit DSL: the fluent API defines many
-         * interfaces that are not useful for bot designers. If you don't want to use val you can use our own
-         * interface IntentVar instead.
          */
-        val greetings = intent("Greetings")
-            .trainingSentence("Hi")
-            .trainingSentence("Hello")
-            .trainingSentence("Good morning")
-            .trainingSentence("Good afternoon")
-            .trainingSentence("What's up");
+        val search = intent("Search")
+                .trainingSentence("Search KEYWORD")
+                .trainingSentence("Search NUM videos of KEYWORD")
+                .trainingSentence("Search NUM videos about KEYWORD")
+                .trainingSentence("Give me KEYWORD")
+                .trainingSentence("Give me NUM videos of KEYWORD")
+                .trainingSentence("Give me NUM videos about KEYWORD")
+                .trainingSentence("Search KEYWORD and give me NUM videos")
+                .parameter("keyword").fromFragment("KEYWORD").entity(any())
+                .parameter("num").fromFragment("NUM").entity(integer());
 
-
-        val howAreYou = intent("HowAreYou")
-                .trainingSentence("How are you?")
-                .trainingSentence("What's up?")
-                .trainingSentence("How do you feel?");
 
         /*
          * Instantiate the platform we will use in the bot definition.
-         * <p>
-         * Instantiating the platform before specifying the bot's states creates a usable reference that can be
-         * accessed in the states, e.g:
-         * <pre>
-         * {@code
-         * myState
-         *   .body(context -> reactPlatform.reply(context, "Hi, nice to meet you!");
-         * }
-         * </pre>
          */
         ReactPlatform reactPlatform = new ReactPlatform();
         /*
          * Similarly, instantiate the intent/event providers we want to use.
-         * <p>
-         * In our example we want to receive intents (i.e. interpreted user inputs) from our react client, so we
-         * create a ReactIntentProvider instance. We also want to receive events from the react client (e.g. when the
-         * client's connection is ready), so we create a ReactEventProvider instance.
-         * <p>
-         * We can instantiate as many providers as we want, including providers from different platforms.
          */
         ReactEventProvider reactEventProvider = new ReactEventProvider(reactPlatform);
         ReactIntentProvider reactIntentProvider = new ReactIntentProvider(reactPlatform);
 
         /*
          * Create the states we want to use in our bot.
-         * <p>
-         * Similarly to platform/provider creation, we create the state variables first, and we specify their content
-         * later. This allows to define circular references between states (e.g. AwaitingQuestion -> HandleWelcome ->
-         * AwaitingQuestion).
-         * <p>
-         * This is not mandatory though, the important point is to have fully specified states when we build the
-         * final bot model.
          */
         val init = state("Init");
         val awaitingInput = state("AwaitingInput");
         val handleWelcome = state("HandleWelcome");
-        val handleWhatsUp = state("HandleWhatsUp");
+        val handleSearch = state("HandleSearch");
 
         /*
          * Specify the content of the bot states (i.e. the behavior of the bot).
@@ -115,7 +110,7 @@ public class MyBot {
                      * ReactEventProvider. The list of events defined in a provider is available in the provider's
                      * wiki page.
                      */
-                    .when(eventIs(ReactEventProvider.ClientReady)).moveTo(awaitingInput);
+                    .when(eventIs(ReactEventProvider.ClientReady)).moveTo(handleWelcome);
 
         awaitingInput
                 .next()
@@ -130,11 +125,13 @@ public class MyBot {
                      * }
                      * </pre>
                      */
-                    .when(intentIs(greetings)).moveTo(handleWelcome)
-                    .when(intentIs(howAreYou)).moveTo(handleWhatsUp);
+                    .when(intentIs(search)).moveTo(handleSearch);
 
         handleWelcome
-                .body(context -> reactPlatform.reply(context, "Hi, nice to meet you!"))
+                .body(context -> reactPlatform.reply(context,
+                        "Hi, I am here to search Youtube videos for you! "+
+                                "You can ask me to search videos about whatever you want. "+
+                                "Feel free to specify the number of videos you want me to search."))
                 .next()
                     /*
                      * A transition that is automatically navigated: in this case once we have answered the user we
@@ -142,8 +139,21 @@ public class MyBot {
                      */
                     .moveTo(awaitingInput);
 
-        handleWhatsUp
-                .body(context -> reactPlatform.reply(context, "I am fine and you?"))
+        handleSearch
+                .body(context -> {
+                    String response = null;
+                    String keyword = (String) context.getIntent().getValue("keyword");
+                    String num_string = (String) context.getIntent().getValue("num");
+                    long num;
+                    if (num_string.equals("")) num = 3L;
+                    else num = Long.parseLong(num_string);
+                    try {
+                        response = getSearchListResponse(youtubeService, keyword, num).toString();
+                    } catch (GeneralSecurityException | IOException e) {
+                        e.printStackTrace();
+                    }
+                    replyYoutubeBot(context, reactPlatform, response, keyword);
+                })
                 .next()
                     .moveTo(awaitingInput);
 
@@ -157,7 +167,7 @@ public class MyBot {
          * Note that every Xatkit bot needs a default fallback state.
          */
         val defaultFallback = fallbackState()
-                .body(context -> reactPlatform.reply(context, "Sorry amigo, I didn't, get it"));
+                .body(context -> reactPlatform.reply(context, "Sorry, I didn't get it"));
 
         /*
          * Creates the bot model that will be executed by the Xatkit engine.
@@ -185,11 +195,11 @@ public class MyBot {
          * Check the corresponding platform's wiki page for further information on optional/mandatory parameters and
          * their values.
          */
-
-        botConfiguration.setProperty("xatkit.dialogflow.projectId", "mybot-fswx");
-        botConfiguration.setProperty("xatkit.dialogflow.credentials.path", "/home/marcos/Downloads/mybot-fswx-f4df98e7a442.json");
+        botConfiguration.setProperty("xatkit.dialogflow.projectId", "youtube-bot-brxn");
+        botConfiguration.setProperty("xatkit.dialogflow.credentials.path", "/home/marcos/Downloads/youtube-bot-brxn-96e49a2a1a12.json");
         botConfiguration.setProperty("xatkit.dialogflow.language", "en-Us");
         botConfiguration.setProperty("xatkit.dialogflow.clean_on_startup", true);
+        //botConfiguration.setProperty("xatkit.server.port", 5000);
 
         XatkitBot xatkitBot = new XatkitBot(botModel, botConfiguration);
         xatkitBot.run();
